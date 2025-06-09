f20a43cb2b58a59a6093baedd3dd26321de06961
fawaid12 <48941679+fawaid12@users.noreply.github.com>
2025-06-09 18:58:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ def preprocess_text(text):
 
 def get_wordcloud(data, title, max_words=20):
     text = ' '.join(data)
-    wordcloud = WordCloud(width=800, height=400, background_color='white').generate(text)
+    wordcloud = WordCloud(width=800, height=400, max_words=max_words, background_color='white').generate(text)
     plt.figure(figsize=(10,5))
     plt.imshow(wordcloud, interpolation='bilinear')
     plt.axis('off')
