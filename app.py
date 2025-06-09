72f2c4a378402787ecc5a747eba6c42844bde949
fawaid12 <48941679+fawaid12@users.noreply.github.com>
2025-06-09 19:23:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -348,7 +348,10 @@ def main():
                 tabel_komentar_populer.append(populer)
     
             if tabel_komentar_populer:
-                df_populer = pd.concat(tabel_komentar_populer, ignore_index=True)[['Topik', 'Komentar']]
+                df_populer_concat = pd.concat(tabel_komentar_populer, ignore_index=True)
+                st.write(df_populer_concat.columns)
+                st.write(df_populer_concat.head())
+
                 st.dataframe(df_populer)
             else:
                 st.info("Tidak ada komentar populer yang bisa ditampilkan.")
